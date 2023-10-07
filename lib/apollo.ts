c72687ae8aecd67b5cbe8e0bc521723f5758cb91
Lucas Paulo <lucaspaulodev@gmail.com>
2023-10-06 23:41:08 -0300

    Adding EXPO_IP_ADDRESS .env variable.

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: `http://192.168.0.6:${process.env.EXPO_API_PORT}`,
+    uri: `http://${process.env.EXPO_IP_ADDRESS}/:${process.env.EXPO_API_PORT}`,
     cache: new InMemoryCache()
 });
