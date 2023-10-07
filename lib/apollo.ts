86e30d9275a4676f26c6685640c5efe7e9ab1a02
Lucas Paulo <lucaspaulodev@gmail.com>
2023-10-06 23:14:01 -0300

    Updating app to use .env variables.

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'http://192.168.0.6:3333/',
+    uri: `http://192.168.0.6:${process.env.EXPO_API_PORT}`,
     cache: new InMemoryCache()
 });
